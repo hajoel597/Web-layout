1060cc4572c047ef374cfbb0f607263e1716a080
hajoel597 <hajoel597@gmail.com>
2023-01-30 20:39:47 +0900

    v1

--- style.js
+++ style.js
@@ -1,2 +1,9 @@
-window.onload = function menu_icon() {
+window.onload = function myfunction() {
+    var x =document.getElementsByClassName("menu_open")
+    if(x.style.display =='block') {
+        x.style.display =='none'
+    } else {
+        x.style.display = 'none';
+      }
 }
+
