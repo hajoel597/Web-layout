abcdfdab0216b846c8a828e39e928fd17053fa3b
hajoel597 <hajoel597@gmail.com>
2023-02-01 22:57:25 +0900

    v.1.0

--- style.js
+++ style.js
@@ -1,8 +1,8 @@
-window.onload = function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+function myFunction() {
+  var x = document.getElementById("nav-links-open");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
+}
